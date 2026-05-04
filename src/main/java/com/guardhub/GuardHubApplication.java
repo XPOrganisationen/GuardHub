44a93241c50a9471df0de506103873182dd46e87
bugintroducer69 <masc1001@stud.ek.dk>
2026-05-04 10:49:23 +0200

    Initial commit
    Contains:
    * Project skeleton
    * CI/CD
    * Containerization.

---
+++ src/main/java/com/guardhub/GuardHubApplication.java
@@ -0,0 +1,13 @@
+package com.guardhub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GuardHubApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GuardHubApplication.class, args);
+    }
+
+}
